ce528cd1f0a0d782cb849450f9b6898d316ae4eb
evnazarchuk <nazarchuk87@gmail.com>
2015-04-16 19:31:51 +0300

    Коррекция

---
+++ protected/views/main/correction_dresses.php
@@ -0,0 +1,16 @@
+<div id="content" align="justify">
+	<div class="post">
+		<div class="entry">
+			<div align = "center" style = "color:#980F39">
+				<form method="post" id="formx" action="javascript:void(null);" onsubmit="correction_dresses();">
+					<h1 style = "color:#980F39">Пошив и коррекция<br></h1>
+					<h2>Оставьте заявку и мы обязательно перезвоним Вам.</h2>
+					<p>Имя*:<br><input type="text" name="name" id="name" size="50" required>
+					<p>Телефон*:<br><input type="text" name="phone" id="phone" size="50" required>
+					<p><input style="width:auto; background:#980F39; color:ffffff" type="submit" value="Записаться">
+				</form> 
+			</div>
+		</div></p>
+	</div>
+</div>
+
